d5ad82dcf1c9d87ad36c2a54e1d45547ac437153
Gustavo <gvsgugu08@gmail.com>
2023-10-10 00:25:33 -0300

    finalizando projeto barbearia

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,23 @@ const menu = document.getElementById('menu_burgue');
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('open');
 });
+
+document.getElementById('top-button').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// formulario
+
+function submitForm(event) {
+  event.preventDefault();  // Evita o envio padrão do formulário
+
+  // Informações para o alerta
+  const alertMessage = "Contato confirmado, logo enviaremos uma mensagem para você.";
+  const confirmButtonText = "Confirmar";
+
+  // Exibe o alerta
+  alert(alertMessage);
+
+  // Redireciona para a seção desejada
+  window.location.href = "/index.html#Location";
+}
